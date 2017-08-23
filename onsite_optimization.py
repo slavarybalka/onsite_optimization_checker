cbeafcb5a9c4cafa468a48aa18cd314dbab3c290
Slava.Rybalka <slavarybalko@gmail.com>
2017-08-23 14:21:22 -0500

    final version

--- onsite_optimization.py
+++ onsite_optimization.py
@@ -1,24 +1,34 @@
 import re
 
+
+
+
+# retrieve page title
 def get_title(page_url):
 
-  title = re.findall('<title>([\s\S]+)<\/title>', page_url)[0] 
-  
+  title = re.findall('<title>[\n\s]*(.+)[\s\n]*<\/title>', page_url)[0]
 
+  print title
   return title
 
+
+
+# retrieve page meta description
 def get_meta_description(page_url):
   try:
-    description = re.findall('name="description"\scontent="(.+)"', page_url)[0]
+    description = re.findall('name="description"\scontent="([\s\S]+?)"', page_url)[0]
     
   except:
     return "Some error retrieving description"
     pass
   return description
 
+
+
+# retrieve h1 tags
 def get_h1(page_url):
   try:
-    h1 = re.findall('<h1.?>([\s\S]+)<\/h1', page_url)
+    h1 = re.findall('<h1.+>(.+)<\/h1>', page_url)
 
   except:
     return "h1 not found"
@@ -26,65 +36,106 @@ def get_h1(page_url):
   return h1
 
 
-
+# retrieve image alt tags
 def get_alt(page_url):
   try:
-    alt = re.findall('alt="(.+)">', page_url)
+    alt = re.findall('alt="(.+?)"', page_url)
   except:
     return "alt not found"
     pass
   return alt
 
+
+
+# retrieve image names
 def get_image_name(page_url):
   try:
-    img_name = re.findall('img.+?src="(.+)"\s', page_url)
+    img_name = re.findall('<img.+?src="(.+?)"', page_url)
   except:
     return "img not found"
     pass
   return img_name
 
+
+
+# check if the keyword is found in the retrieved page elements
 def check_occurence(keyword, retrieved_title, retrieved_meta_description, retrieved_h1, retrieved_alts, retrieved_image_names):
   global counter
   counter = 0
 
-  try:
-    if keyword in retrieved_title.lower():
-      counter += 20
-      print "Keyword found in title (exact match)"
-      
 
-    if keyword in retrieved_meta_description.lower():
-      counter += 20
-      print "Keyword found in meta description (exact match)"
-      
+  # Check if keyword is in title
+  if keyword in retrieved_title.lower():
+    counter += 20
+    print "Keyword found in title (exact match)", retrieved_title
+    title_marker = 'yes'
+  else:
+    print "Keyword '{0}' not found in title: {1}".format(keyword, retrieved_title.strip())
+    title_marker = 'no'
 
-    if retrieved_h1:   
+  # Check if keyword is in meta description
+  if keyword in retrieved_meta_description.lower():
+    counter += 20
+    print "Keyword found in meta description (exact match)", retrieved_meta_description
+    meta_desc_marker = 'yes'
+  else:
+    print "Keyword '{0}'' not found in meta description: {1}".format(keyword, retrieved_meta_description.strip())
+    meta_desc_marker = 'no'
+
+  # Check if keyword is in h1
+  if retrieved_h1:
+      
+    if len(retrieved_h1) > 1:
       
       if any(keyword in b.lower().replace('-', ' ') for b in retrieved_h1):
         counter += 20
-        print "Keyword found in h1 (exact match)"
+        print "Keyword found in h1 (exact match)", retrieved_h1
+        h1_marker = 'yes'
+      else:
+        print "Keyword '{0}'' not found in h1: {1}".format(keyword, retrieved_h1)
+        h1_marker = 'no'      
+    if len(retrieved_h1) == 1:
       
-
-    if retrieved_alts:
-
-      if any(keyword in i.lower().replace('-', ' ').replace('_', ' ') for i in retrieved_alts):
+      
+      if keyword in retrieved_h1[0].lower().replace('-', ' '):
         counter += 20
-        print "Keyword found in image alt tags (exact match)"
-      
+        print "Keyword found in h1 (exact match)", retrieved_h1
+        h1_marker = 'yes'
+      else:
+        print "Keyword '{0}'' not found in h1: {1}".format(keyword, retrieved_h1[0].strip()) 
+        h1_marker = 'no'
+  else:
+    h1_marker = 'no'
 
-    if retrieved_image_names:
-      
-      if any(keyword in z.lower().replace('-', ' ') for z in retrieved_image_names):
-        counter += 20
-        print "Keyword found in image name (exact match)"
-      
+  # Check if keyword is in image alt tags
+  if retrieved_alts:
 
-  except: 
-    print "Write something."    
+    if any(keyword in i.lower().replace('-', ' ').replace('_', ' ') for i in retrieved_alts):
+      counter += 20
+      print "Keyword found in image alt tags (exact match)"#, retrieved_alts
+      img_alt_marker = 'yes'
+    else:
+      print "Keyword not in image alts."
+      img_alt_marker = 'no'
+   
+  # Check if keyword is in image names  
+  if retrieved_image_names:
+    
+    if any(keyword in z.lower().replace('-', ' ') for z in retrieved_image_names):
+      counter += 20
+      print "Keyword found in image name (exact match)"#, retrieved_image_names
+      img_name_marker = 'yes'
+    else:
+      print "Keyword not in image names."
+      img_name_marker = 'no'
+
+
   
 
-  print "Onsite optimization for '{0}': {1}".format(keyword, counter) + '%' 
-  return counter
+  outt = "Onsite optimization for '{0}': {1}".format(keyword, counter) + '%\n' 
+  print outt
+  return [outt, counter, title_marker, meta_desc_marker, h1_marker, img_name_marker, img_alt_marker]
+
 
 
 
--- onsite_seo_checker.py
+++ onsite_seo_checker.py
@@ -1,28 +1,87 @@
-# https://moz.com/ugc/how-i-built-an-online-community-from-0-to-1500-members
-
 import pandas as pd
 import numpy as np
 import requests
+import csv
 import re
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
 from onsite_optimization import get_title, get_meta_description, get_h1, get_alt, get_image_name, check_occurence
 
-url = raw_input("Enter URL to check: ")
-keyword = raw_input("Enter Keyword to check: ")
+list_of_programs = open("program_pages1", 'r').readlines()
+export_list = []
+final_export_list = []
 
-#check if the keyword is in Title, Meta Description, H1, alt tag, image name.
+all_programs = len(list_of_programs)
+cnter = 0
+
+for program in list_of_programs:
+	try:
+		probe = "http://api.semrush.com/?type=url_organic&key={Insert your API key here}&display_limit=100&export_columns=Ph,Po,Nq,Cp,Co,Tr,Tc,Nr,Td&url=" + program.strip() + "&database=us"
+
+		cnter += 1
+		print "Checking program page {0} of {1}".format(cnter, all_programs)
+
+		#print probe
+		print program
+
+		semrush = requests.get(probe)
+		for line in semrush.text.split("\n")[1:]:
+
+			#print line
+			#print program
+			#export_list.append(line)
+			x = line.split(';')
+			#export_list.append(x)
+			line_1 = [program, x[0], x[1], x[2]]
+			
+			export_list.append([program.strip(), x[0], x[1], x[2]])
+	    	
+	except:
+		print "ERROR WHEN QUERYING SEMRUSH"
+		pass
+
+
+		
+
+	
+#print export_list
+
+for item in export_list:
+	#print item
+	if int(item[2]) > 10 and int(item[2]) < 21:
+		print item
+		url = item[0]
+		keyword = item[1]
 
 
 
+		#check if the keyword is in Title, Meta Description, H1, alt tag, image name.
+		print url
+		result = requests.get(url).text.lower()
+		#print result
 
-result = requests.get(url).text
+		print '\n'
+		retrieved_title = get_title(result)
+		retrieved_meta_description = get_meta_description(result)
+		retrieved_h1 = get_h1(result)
+		retrieved_alts = get_alt(result)
+		retrieved_image_names = get_image_name(result)
+
+		z = check_occurence(keyword, retrieved_title, retrieved_meta_description, retrieved_h1, retrieved_alts, retrieved_image_names)
+		print z
+		final_export_list.append([url, keyword, item[2], item[3], str(z[1])+'%', z[2], z[3], z[4], z[5], z[6]])
+
+for i in final_export_list:
+	print i
 
 
-print '\n'
-retrieved_title = get_title(result)
-retrieved_meta_description = get_meta_description(result)
-retrieved_h1 = get_h1(result)
-retrieved_alts = get_alt(result)
-retrieved_image_names = get_image_name(result)
+with open('onsite_seo_results.csv', 'wb') as csvfile:
+    x_writer = csv.writer(csvfile, delimiter=',')
+    x_writer.writerow(['URL','Keyword','Position','Search Volume','Level of Optimization','KW in Title', 'KW in Meta Description', 'KW in h1', 'KW in Image Name', 'KW in Image Alts']) #writing headers
+    for final_export_data in final_export_list:
+    	#print export_data
+        x_writer.writerow(final_export_data)
 
-check_occurence(keyword, retrieved_title, retrieved_meta_description, retrieved_h1, retrieved_alts, retrieved_image_names)
-#check_occurence(keyword, retrieved_title, retrieved_meta_description)
